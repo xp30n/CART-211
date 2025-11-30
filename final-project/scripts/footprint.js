f1f06d600ddb64fbf21ccfc924f1882cbb0f3465
Lia <aliyahrenomeron7@gmail.com>
2025-11-30 18:43:43 -0500

    added a separate js file for page 6

--- final-project/scripts/footprint.js
+++ final-project/scripts/footprint.js
@@ -1,86 +1,16 @@
-/*=================================================
-=              IMAGE CAROUSEL SCRIPT              =
-==================================================*/
-
-// const galleryContainer = document.querySelector(".gallery-container");
-// const galleryControlsContainer = document.querySelector(".gallery-controls");
-// const galleryControls = ["previous", "next"];
-// const galleryItems = document.querySelectorAll(".gallery-item");
-
-// class Carousel {
-//   constructor(container, items, controls) {
-//     this.carouselContainer = container;
-//     this.carouselControls = controls;
-//     this.carouselArray = [...items];
-//     this.totalItems = this.carouselArray.length;
-//   }
-
-//   updateGallery() {
-//     this.carouselArray.forEach((el, index) => {
-//       for (let i = 1; i <= this.totalItems; i++) {
-//         el.classList.remove(`gallery-item-${i}`);
-//       }
-//     });
-//     this.carouselArray.forEach((el, i) => {
-//       el.classList.add(`gallery-item-${i + 1}`);
-//     });
-//   }
-
-//   setCurrentState(direction) {
-//     if (direction.className.includes("previous")) {
-//       this.carouselArray.unshift(this.carouselArray.pop());
-//     } else {
-//       this.carouselArray.push(this.carouselArray.shift());
-//     }
-//     this.updateGallery();
-//   }
-
-//   setControls() {
-//     this.carouselControls.forEach((control) => {
-//       let button = document.createElement("button");
-//       button.className = `gallery-controls-${control}`;
-//       button.innerText = control;
-//       galleryControlsContainer.appendChild(button);
-//     });
-//   }
-
-//   useControls() {
-//     const triggers = [...galleryControlsContainer.childNodes];
-//     triggers.forEach((control) => {
-//       control.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         this.setCurrentState(control);
-//       });
-//     });
-//   }
-// }
-
-// const exampleCarousel = new Carousel(
-//   galleryContainer,
-//   galleryItems,
-//   galleryControls
-// );
-
-// exampleCarousel.setControls();
-// exampleCarousel.useControls();
-
-/*=================================================
-=              PAGE 5 TERMINAL SCRIPT             =
-==================================================*/
-
 const terminal = document.getElementById("terminal");
 const inputField = document.getElementById("user-input");
 
 // Allows the user to type in the terminal without having to click it when the page first loads
 if (inputField) {
-  window.onLoad = () => inputField.focus;
+  window.onload = () => inputField.focus();
 }
 
 // TYPING EFFECT FOR TEXT
 function typeText(text, delay = 20) {
   return new Promise((resolve) => {
     let i = 0;
-    let line = document.createElement(div);
+    let line = document.createElement("div");
     terminal.appendChild(line);
 
     function typeChar() {
@@ -108,7 +38,7 @@ function print(text) {
 
 async function processCommand(cmd) {
   cmd = cmd.toUpperCase().trim();
-  print(`EXOS > ${cmd}`);
+  print(`EX0S > ${cmd}`);
 
   // Commands that will display based on the word that is entered into the terminal
   if (cmd === "HELP") {
@@ -144,6 +74,7 @@ async function processCommand(cmd) {
     setTimeout(() => {
       window.location.href = "#";
     }, 800);
+
   } else {
     await typeText(`UNKNOWN COMMAND: ${cmd}`);
   }
@@ -151,11 +82,11 @@ async function processCommand(cmd) {
 
 // Allows the user to press "Enter" to input commands
 if (inputField) {
-    inputField.addEventListener("keydown", function(e) {
-        if (e.key === "Enter") {
-            let command = inputField.value;
-            inputField.value = "";
-            processCommand(command);
-        }
-    });
+  inputField.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+      let command = inputField.value;
+      inputField.value = "";
+      processCommand(command);
+    }
+  });
 }
